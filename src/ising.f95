f54dc8a70ad2e1b339ba3e6738825cc6f0e33c8a
Justin Browne <brownej2@msu.edu>
2015-02-10 11:59:26 -0500

    added magnetization function removed nx and ny from function arguments

--- src/ising.f95
+++ src/ising.f95
@@ -7,10 +7,10 @@ program ising
   integer, allocatable :: sigma(:,:)
   real, parameter      :: INTER = 1
   real, parameter      :: FIELD = 0
-  real, parameter      :: BETA  = 1
+  real, parameter      :: BETA  = 1E-2
   real, parameter      :: E     = exp(1.0)
 
-  nx = 500
+  nx = 100
   ny = 100
 
   allocate (sigma(nx,ny))
@@ -35,11 +35,17 @@ contains
     ny = size(sigma,2)
 
     ! Initialize sigma randomly
-    allocate(tmparr(nx,ny))
-    call init_random_seed()
-    call random_number(tmparr)
-    sigma = floor(tmparr*2)*2-1
-    deallocate(tmparr)
+!    allocate(tmparr(nx,ny))
+!    call init_random_seed()
+!    call random_number(tmparr)
+!    sigma = floor(tmparr*2)*2-1
+!    deallocate(tmparr)
+    ! Initialize sigma as all up
+    do j=1,ny
+      do i=1,nx
+        sigma(i,j)=1
+      end do
+    end do
 
     ! Print out initial conditions
     call plot_lattice(sigma)
@@ -59,27 +65,28 @@ contains
       ! Else, stay with probability of e^{-beta*(H_1-H_0)}
       newsigma = sigma
       newsigma(i,j) = -sigma(i,j)
-      Ediff = neighb_contrib(i,j,nx,ny,newsigma) + field_contrib(i,j,newsigma) &
-              - neighb_contrib(i,j,nx,ny,sigma) - field_contrib(i,j,sigma)
+      Ediff = neighb_contrib(i,j,newsigma) + field_contrib(i,j,newsigma) &
+              - neighb_contrib(i,j,sigma) - field_contrib(i,j,sigma)
       call random_number(tmp)
       if (tmp < E**(-BETA*Ediff)) then
         sigma = newsigma
       end if
+
+      write(*,*) magnetization(sigma)
     end do
     deallocate(newsigma)
-    !call sigmaprint(nx,ny,sigma)
+    !call sigmaprint(sigma)
     call plbop()
     call plot_lattice(sigma)
     call pleop()
   end subroutine montecarlo
 
-  subroutine sigmaprint(nx,ny,sigma)
-    integer, intent(in) :: nx,ny
-    integer, intent(in) :: sigma(nx,ny)
+  subroutine sigmaprint(sigma)
+    integer, intent(in) :: sigma(:,:)
     integer             :: i,j
 
-    do j=1,ny
-      do i=1,nx
+    do j=1,size(sigma,2)
+      do i=1,size(sigma,1)
         if (sigma(i,j) == 1) then
           write(*,"(a)",advance='no') "+"
         end if
@@ -91,10 +98,14 @@ contains
     end do
   end subroutine sigmaprint
 
-  function neighb_contrib(i,j,nx,ny,sigma) result(contrib)
-    integer, intent(in) :: i,j,nx,ny
-    integer, intent(in) :: sigma(nx,ny)
+  function neighb_contrib(i,j,sigma) result(contrib)
+    integer, intent(in) :: i,j
+    integer, intent(in) :: sigma(:,:)
+    integer             :: nx,ny
     real                :: contrib
+
+    nx=size(sigma,1)
+    ny=size(sigma,2)
 
     contrib = 0
 
@@ -116,7 +127,7 @@ contains
 
   function field_contrib(i,j,sigma) result(contrib)
     integer, intent(in) :: i,j
-    integer, intent(in) :: sigma(nx,ny)
+    integer, intent(in) :: sigma(:,:)
     real                :: contrib
 
     contrib = 0
@@ -125,20 +136,34 @@ contains
     contrib = contrib - FIELD*sigma(i,j)
   end function field_contrib
 
-  function hamiltonian(nx,ny,sigma) result(energy)
-    integer, intent(in) :: nx,ny
+  function hamiltonian(sigma) result(energy)
     integer, intent(in) :: sigma(nx,ny)
     integer             :: i,j
     real                :: energy
 
     energy = 0
 
-    do j=1,ny
-      do i=1,nx
+    do j=1,size(sigma,2)
+      do i=1,size(sigma,1)
         ! neigb_contrib is halved to prevent double counting
-        energy = energy + 0.5*neighb_contrib(i,j,nx,ny,sigma)
+        energy = energy + 0.5*neighb_contrib(i,j,sigma)
         energy = energy + field_contrib(i,j,sigma)
       end do
     end do
   end function hamiltonian
+
+  function magnetization(sigma) result(mag)
+    integer, intent(in) :: sigma(:,:)
+    integer :: i,j
+    integer :: m
+    real(8) :: mag
+
+    m = 0
+    do j=1,size(sigma,2)
+      do i=1,size(sigma,1)
+        m=m+sigma(i,j)
+      end do
+    end do
+    mag = dble(m)/size(sigma)
+  end function magnetization
 end program ising
