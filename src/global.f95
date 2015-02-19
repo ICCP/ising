845249ba3829bfecb33d24e101adaa18fba02a9f
Scott O'Connor <oconn220@msu.edu>
2015-02-19 00:10:58 -0500

    latest submittion of ising project

--- src/global.f95
+++ src/global.f95
@@ -2,6 +2,7 @@ module global
 
   implicit none
 
+  integer :: t_avg,temp_points
   integer :: latticeSize,iters,n1,n2
   integer, allocatable :: lattice(:,:)
   real(8) :: rand_loc(2)
@@ -11,7 +12,7 @@ module global
   real(8) :: del_e
   real(8) :: h(2)
   real(8) :: j                  !magnetic coefficients
-  real(8) :: temp
+  real(8) :: min_temp,max_temp
   real(8) :: k                  !boltzman constant
   real(8) :: B                  !combination of temp and boltzman
 
--- src/ising.f95
+++ src/ising.f95
@@ -9,45 +9,67 @@ program ising
   integer,external :: hfield
 
   !Variables for main routine
-  integer :: ii
+  integer :: ii,jj
   integer :: tot_energy
+  real(8) :: energy_avg
+  real(8) :: hfeild_avg
+  real(8) :: temp
 
   !output files
-  open (unit = 1, file = 'energy.out',status = 'unknown')
-  open (unit = 2, file = 'hfield.out',status = 'unknown')
+  open (unit = 1, file = 'energy.out',     status = 'unknown')
+  open (unit = 2, file = 'hfield.out',     status = 'unknown')
+  open (unit = 3, file = 'ising.inp',      status = 'unknown')
+  open (unit = 4, file = 'hfield_avg.out', status = 'unknown')
+  open (unit = 5, file = 'energy_avg.out', status = 'unknown')
 
   !initializing variables
   ii = 0                 !Iteration Variable
+  jj = 0                 !Averages Variables
   energy_sum = 0         !Energy Sum
-  
+  rand_loc = 2           !Init Random Location Variable
+
   call ReadInput
   
-  rand_loc = 2           !Init Random Location Variable
-  j = 1.d0               !magnetic coeffient
-  B = 1/(k*temp)         !constant of temp and boltman
-
   allocate(lattice(0:latticeSize + 1,0:latticeSize + 1))
   lattice = 0
   lattice(1:latticeSize, 1:latticeSize) = 1
   
   !main program 
-  tot_energy = energy(lattice,latticeSize)!Calculate Chagnge total energy
-  do ii = 0,iters
-      !generate a random location 
-      Call random_number(rand_loc)
-      rand_loc = rand_loc*(latticeSize)
-      rand_loc_int = ceiling(rand_loc)
+  
+  do jj = 0,temp_points
+      print*, 'Current Temp Point:',jj
+      hfeild_avg=0
+      energy_avg=0
+      tot_energy=0
+      temp=min_temp+((max_temp-min_temp)/temp_points)*jj
+      B = 1/(k*temp)         !constant of temp and boltman
+      tot_energy = energy(lattice,latticeSize)!Calculate Chagnge total energy
+      do ii = 0,iters
+          !generate a random location 
+          Call random_number(rand_loc)
+          rand_loc = rand_loc*(latticeSize)
+          rand_loc_int = ceiling(rand_loc)
+          
+          call del_energy !calculate change in energy
+          call flip_bit   !Perform metropolis test
+          
+          !Write results to file
+          if (ii > iters-1) then   
+            write (1,*),ii, energy_sum 
+            write (2,*),ii, sum(lattice)/float(latticeSize**2)
+          end if
+          if (ii > iters-t_avg) then
+              hfeild_avg=hfeild_avg+(1/(float(ii)+1))*(sum(lattice)/float(latticeSize**2)-hfeild_avg)
+              energy_avg=energy_avg+(1/(float(ii)+1))*(tot_energy/float(latticeSize*2)-energy_avg)
+          end if 
+      end do
       
-      call del_energy !calculate change in energy
-      call flip_bit   !Perform metropolis test
-      
-      !Write results to file
-      write (1,*),ii, energy_sum 
-      write (2,*),ii, sum(lattice)/float(latticeSize**2)
+      !writing averages to file
+      write(4,*),temp,hfeild_avg
+      write(5,*),temp,energy_avg
 
-  end do
+  end do 
 end program 
-
 
 !------------------------------------------------------------------------------
 subroutine del_energy!{{{
@@ -106,14 +128,16 @@ end function !}}}
 !------------------------------------------------------------------------------
 subroutine ReadInput !{{{
     
-    use global, only: latticeSize,iters,k,temp
-
-    open(3,file="ising.inp")
+    use global, only: latticeSize,iters,k,min_temp,max_temp,temp_points, &
+                      t_avg,k,j  
 
     read(3,*) latticeSize
     read(3,*) iters
+    read(3,*) min_temp,max_temp,temp_points
+    read(3,*) t_avg
     read(3,*) k
-    read(3,*) temp
+    read(3,*) j
+  
 
     close(3)
 
